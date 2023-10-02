6dc3b523416eb1dbce30aeb881a9caf6beb02414
sergii <sk2009@gmail.com>
2023-10-02 23:20:11 +0300

    tried content function

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-const form = document.querySelector("form");
+import OpenAI from 'openai';
+
+// const form = document.querySelector("form");
+
+const getObjectFromLocalStorage = async function (key) {
+    return new Promise((resolve, reject) => {
+        try {
+            chrome.storage.local.get(key, function (value) {
+                resolve(value[key]);
+            });
+        } catch (ex) {
+            reject(ex);
+        }
+    });
+};
+
+
+let settings = await getObjectFromLocalStorage("settings");
+let api_key = settings.apiKey;
+let openai = new OpenAI({ apiKey: api_key, dangerouslyAllowBrowser: true });
 
 function save_settings() {
     let settings = {
         apiKey: document.querySelector("#api-key").value,
         model: document.querySelector("#model").value
-        // blacklist: document.querySelector("#blacklist").value
     }
     chrome.storage.local.set({ settings }, () => {
         console.log("settings saved");
@@ -20,20 +38,98 @@ async function getCurrentTab() {
 
 function getTitle() { console.log("document.title", document.title); }
 
+async function sendPageContent(openai) {
+
+    console,log("sendPageContent");
+
+    const saveObjectInLocalStorage = async function (obj) {
+        return new Promise((resolve, reject) => {
+            try {
+                chrome.storage.local.set(obj, function () {
+                    resolve();
+                });
+            } catch (ex) {
+                reject(ex);
+            }
+        });
+    };
+
+    // let settings = await getObjectFromLocalStorage("settings");
+    // let api_key = settings.apiKey;
+
+    // console.log("content script", api_key);
+
+    // let openai = new OpenAI({ apiKey: api_key, dangerouslyAllowBrowser: true });
+
+    // let settings = await getObjectFromLocalStorage("settings");
+
+    // let blacklist = settings.blacklist.split('\n');
+    // console.log("blacklist", settings.blacklist, blacklist);
+    // for (let b of blacklist) {
+    //     console.log("check b", b, window.location.href, window.location.href.includes(b));
+    //     if (window.location.href.includes(b)) {
+    //         console.log("page is blacklisted", window.location.href, b);
+    //         blockPage("page is blacklisted", window.location.href);
+    //         return;
+    //     }
+    // }
+
+    const textContent = document.body.innerText;
+    let text = window.location.href + '\n' + textContent.substring(0, 2000);
+
+    let prompt =
+        `You are an expert in cataloguing, ranking, archiving and retrieving web pages.
+For a given text from a web page, please write:
+- list of descriptive keywords for future retrieval
+- rank this text on a scale 1-10 for being relevant and interesting for future retrieval
+- select category, sub-category, sub-sub-category, sub-sub-sub-category for the content
+Please keep tags and categories as short and concise as possible.
+print output in following JSON format:
+{tags: [...], rating: ..., categories: [category, sub-category, sub-sub-category, sub-sub-sub-category]}
+===
+{text}`;
+
+    console.log('prompt:', prompt);
+    console.log("use model", settings.model);
+
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: prompt }],
+        model: settings.model,
+    });
+
+    let content = completion.choices[0].message.content;
+
+    console.log("chat response:", a);
+
+    chrome.runtime.sendMessage({ message: 'chat response' },
+        (response) => console.log(response));
+
+    saveObjectInLocalStorage({
+        [window.location.href]: {
+            href: window.location.href,
+            content: content
+        }
+    });
+}
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    console.log("chrome.runtime.onMessage", message);
+});
 
 async function add_bookmark() {
 
     console.log("add pointerdown", scriptFileName);
-    
+
     let tab = await getCurrentTab();
 
     console.log("tab", tab);
 
-    console.log("executeScript", getTitle)
+    console.log("executeScript", sendPageContent)
 
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        func: getTitle
+        func: sendPageContent,
+        args : [ openai ]
         // files: [scriptFileName]
     });
 }
@@ -55,6 +151,5 @@ document.getElementById("clear").addEventListener('pointerdown', () => {
 chrome.storage.local.get("settings", (data) => {
     document.querySelector("#api-key").value = data.settings.apiKey || "";
     document.querySelector("#model").value = data.settings.model || "";
-    // document.querySelector("#blacklist").value = data.settings.blacklist || "";
     console.log("settings read", data.settings);
 });
